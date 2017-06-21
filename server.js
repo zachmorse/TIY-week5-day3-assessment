77390b9a4fd15ad0804b10787deb437da6b1672d
Zach Morse <zacharymorse@gmail.com>
2017-06-21 18:49:12 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mustacheExpress = require("mustache-express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./public");
+app.set("view engine", "mustache");
+var toDoList = [];
+
+// MIDDLEWARE
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ROUTES
+
+app.use("/", express.static("./public"));
+
+var todos = [];
+
+app.get("/", function(req, res) {
+  res.render("index", { todos: todos });
+});
+
+app.post("/", function(req, res) {
+  todos.push(req.body.listItem);
+  res.redirect("/");
+});
+
+// LISTENER
+
+app.listen(port, function(req, res) {
+  console.log("Server running on port " + port);
+});
